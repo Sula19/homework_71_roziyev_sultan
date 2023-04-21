561e579a5c055986ffd44a429c14ee01533b3799
Sulatn Roziyev <jjo152065@gmail.com>
2023-04-21 15:24:15 +0600

    Мелкие правки

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PostAPIView, LikeAPIView
+from .views import PostAPIView
 
 router = routers.DefaultRouter()
 router.register(r'post', PostAPIView)
-router.register(r'like', LikeAPIView)
-
 
 urlpatterns = [
     path('auth/', include('rest_framework.urls'), name='rest_framework'),
