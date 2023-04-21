293a62a545091034a579987d544560e303730e14
Sulatn Roziyev <jjo152065@gmail.com>
2023-04-21 16:05:01 +0600

    Мелкие правки

--- core/api/permission.py
+++ core/api/permission.py
@@ -1,7 +1,11 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 from instagram.models import Post
 
 
 class IsCurrentUser(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
